1b51460c9dce3b05cf4072b8a7d8d39e83a0111d
Yash Shinde <shindeyash70@gmail.com>
2024-12-02 15:49:40 +0530

    change

--- Addition.java
+++ Addition.java
@@ -1,1 +1,14 @@
-public
+public class Addition{
+	int a,b;
+	public Addition(int a,int b) {
+		this.a=a;
+		this.b=b;
+		System.out.println(a+b);
+		
+	}
+	
+	public static void main(String[] args) {
+		Addition a=new Addition(5, 8);
+		System.out.println(a.a+a.b);
+	}
+}
